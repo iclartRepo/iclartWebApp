d468f095177a0664fd2e59784d918dc4af0d7609
asus <asus@GUERRIC-PC>
2017-02-20 22:23:50 +0800

    Added UI Changes to Form for client adding

--- Iclart.WebApp/wwwroot/src/clientManagement/clientForm/clientForm.component.js
+++ Iclart.WebApp/wwwroot/src/clientManagement/clientForm/clientForm.component.js
@@ -16,6 +16,23 @@
     function ControllerController() {
         var vm = this;
 
+        vm.page = 1;
+
+        vm.setPage = function (page) {
+            vm.page = page;
+        }
+        vm.changePage = function (increment) {
+            if (increment == -1) {
+                if (vm.page != 1) {
+                    vm.page -= 1;
+                }
+            }
+            else {
+                if (vm.page != 2) {
+                    vm.page += 1;
+                }
+            }
+        }
         vm.$onInit = function () {
             
         }
